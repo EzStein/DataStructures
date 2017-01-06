27faab99490e1d24cf8d604ef95373656f829bbc
Ezra Stein <ezrastein@Ezras-Air.home>
2017-01-06 17:50:55 -0500

    initial removal

--- src/avl_tree.c
+++ src/avl_tree.c
@@ -53,7 +53,70 @@ void avl_tree_add(avl_tree_t * tree, void * key) {
 }
 
 void avl_tree_remove(avl_tree_t * tree, void * key) {
+  avl_tree_node_t * node = tree->root;
+  avl_tree_node_t ** parents_child_ref;
+  avl_tree_node_t * tmp_node;
+  void * tmp_key;
 
+  int8_t compare;
+  while(node) {
+    compare = tree->compare(key, node->key);
+    if(compare < 0) {
+      node = node->left;
+    } else if (compare > 0) {
+      node = node->right;
+    } else {
+      break;
+    }
+  }
+  if(!node) return;
+
+  /*We must now remove node*/
+  if(node->parent) {
+    if(compare < 0)
+      parents_child_ref = &(node->parent->left);
+    else
+      parents_child_ref = &(node->parent->right);
+  }
+
+  if(node->left && node->right) {
+    tmp_node = node->right;
+    parents_child_ref = &(node->right);
+    while(tmp_node->left) {
+      parents_child_ref = &(tmp_node->left);
+      tmp_node = tmp_node->left;
+    }
+    tmp_key = node->key;
+    node->key = tmp_node->key;
+    tmp_node->key = tmp_key;
+
+    node = tmp_node;
+    *parents_child_ref = node->right;
+    if(node->right)
+      node->right->parent = node->parent;
+  } else if(node->left && !node->right) {
+    node->left->parent = node->parent;
+    if(!node->parent)
+      tree->root = node->left;
+    else
+      *parents_child_ref = node->left;
+  } else if(!node->left && node->right) {
+    node->right->parent = node->parent;
+    if(!node->parent)
+      tree->root = node->right;
+    else
+      *parents_child_ref = node->right;
+  } else {
+    if(!node->parent)
+      tree->root = NULL;
+    else
+      *parents_child_ref = NULL;
+  }
+
+  if(tree->free_function)
+    tree->free_function(node->key);
+  free(node->key);
+  free(node);
 }
 
 int8_t avl_tree_contains(avl_tree_t * tree, void * key) {
